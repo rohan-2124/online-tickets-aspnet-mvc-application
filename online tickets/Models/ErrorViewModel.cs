5c58a694b494aca7de492569d81e8879a079c33d
ROHAN <ROHAN@DESKTOP-TR9DICH>
2023-03-14 21:30:09 +0530

    Add project files.

---
+++ online tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace online_tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
